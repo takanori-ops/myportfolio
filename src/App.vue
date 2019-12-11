d9e808e3c53536c3857e5fe841f5339fa997506c
takanori-ops <takanori20401@gmail.com>
2019-12-11 23:23:34 +0900

    ベース作成

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,310 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="wrapper">
+    <nav id="gnav" class="fixed">
+      <ul>
+        <li><a href="#works">WORKS</a></li>
+        <li><a href="#feature">FEATURE</a></li>
+        <li><a href="#about">ABOUT</a></li>
+        <li><a href="#skill">SKILL</a></li>
+        <li><a href="#other">OTHER</a></li>
+        <li><a href="#contact">CONTACT</a></li>
+      </ul>
+    </nav>
+    <header id="header">
+      <div class="inner">
+        <h1 class="logo">
+          <div class="logo-title">Work Space</div>
+          <div class="logo-desc">taka</div>
+        </h1>
+      </div>
+    </header>
+    <main id="contents">
+      <section class="section" id="works">
+        <div class="inner">
+          <div class="section-title-area">
+            <h2 class="section-title">WORKS</h2>
+            <div class="section-intro">
+              <div class="section-text">
+                ただいま準備中です。<br class="pc">こちらも準備中です。
+              </div>
+            </div>
+          </div>
+          <div class="section-body card-wrapper my-gallery">
+            <div class="card">
+              <a href="img/works/company.jpg" data-size="960x720" class="card-image hover-action-01">
+                <img class="lazyload fadein" src="img/placeholder4x3.png" data-src="img/works/company.jpg" alt="" />
+              </a>
+              <div class="card-caption">
+                <div class="card-caption-title">ただいま準備中です。</div>
+                <div class="card-caption-info">Design / Coding(Responsive) / WordPress</div>
+                <div class="card-caption-url"><a class="url-link" href="https://yahoo.co.jp" target="_blank">Link</a>
+                </div>
+              </div>
+            </div>
+            <div class="card">
+              <a href="img/works/hairsalon.jpg" data-size="960x720" class="card-image hover-action-01">
+                <img class="lazyload fadein" src="img/placeholder4x3.png" data-src="img/works/hairsalon.jpg" alt="" />
+              </a>
+              <div class="card-caption">
+                <div class="card-caption-title">ただいま準備中です。</div>
+                <div class="card-caption-info">Design / Coding(Responsive) </div>
+                <div class="card-caption-url"><a class="url-link" href="https://yahoo.co.jp" target="_blank">Link</a>
+                </div>
+              </div>
+            </div>
+            <div class="card">
+              <a href="img/works/girly.jpg" data-size="960x720" class="card-image hover-action-01">
+                <img class="lazyload fadein" src="img/placeholder4x3.png" data-src="img/works/girly.jpg" alt="" />
+              </a>
+              <div class="card-caption">
+                <div class="card-caption-title">ただいま準備中です。</div>
+                <div class="card-caption-info">Design</div>
+              </div>
+            </div>
+            <div class="card">
+              <a href="img/works/school.jpg" data-size="960x720" class="card-image hover-action-01">
+                <img class="lazyload fadein" src="img/placeholder4x3.png" data-src="img/works/school.jpg" alt="" />
+              </a>
+              <div class="card-caption">
+                <div class="card-caption-title">ただいま準備中です。</div>
+                <div class="card-caption-info">Design</div>
+              </div>
+            </div>
+            <div class="card">
+              <a href="img/works/game.jpg" data-size="960x720" class="card-image hover-action-01">
+                <img class="lazyload fadein" src="img/placeholder4x3.png" data-src="img/works/game.jpg" alt="" />
+              </a>
+              <div class="card-caption">
+                <div class="card-caption-title">ただいま準備中です。</div>
+                <div class="card-caption-info">Design</div>
+              </div>
+            </div>
+            <div class="card">
+              <a href="img/works/fashion.jpg" data-size="960x720" class="card-image hover-action-01">
+                <img class="lazyload fadein" src="img/placeholder4x3.png" data-src="img/works/fashion.jpg" alt="" />
+              </a>
+              <div class="card-caption">
+                <div class="card-caption-title">ただいま準備中です。</div>
+                <div class="card-caption-info">Design</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section class="section" id="feature">
+        <div class="inner">
+          <div class="section-title-area">
+            <h2 class="section-title">FEATURE</h2>
+            <div class="section-intro">
+              ただいま準備中です。
+            </div>
+          </div>
+          <div class="section-body">
+            <div class="section-image">
+              <a href="https://webdesigner-go.com/" class="hover-action-01" target="_blank"><img
+                  src="img/feature/feature.jpg" alt=""></a>
+            </div>
+            <div class="section-desc">
+              <div class="section-text">
+                <span class="text-bold">サイト名：</span><br>サイト名が入る<br><a href="#"
+                  target="_blank">https://●●●.com</a><br><br>
+                <span class="text-bold">担当：</span><br>Design / Coding (Responsive) / WordPress /
+                Writing<br>
+                <br>
+                <span
+                  class="text-bold">コメント：</span><br>ただいま準備中です。<br>ただいま準備中です。<br><br>ただいま準備中です。<br>ただいま準備中です。<br>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section class="section" id="about">
+        <div class="inner">
+          <div class="section-title-area">
+            <h2 class="section-title">ABOUT</h2>
+            <div class="section-intro">
+              経歴や職歴
+            </div>
+          </div>
+          <div class="section-body">
+            <div class="section-image">
+              <img class="lazyload fadein" data-src="img/about/profile.jpg" alt="">
+            </div>
+            <div class="section-desc">
+              <div class="section-text">
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section class="section" id="skill">
+        <div class="inner">
+          <div class="section-title-area">
+            <h2 class="section-title">SKILL</h2>
+            <div class="section-intro">
+              よろしくお願いします。
+            </div>
+          </div>
+          <div class="section-body card-wrapper">
+            <div class="card">
+              <div class="skill-icon">
+                <img src="img/skill/icon_skill.png" alt="">
+              </div>
+              <div class="skill-desc">
+                <h3 class="skill-desc-title">HTML5/CSS3/JavaScript</h3>
+                <div class="rating">
+                  <div class="rate rate2"></div>
+                </div>
+                <div class="section-text">
+                  ただいま準備中です。</div>
+              </div>
+            </div>
+            <div class="card">
+              <div class="skill-icon">
+                <img src="img/skill/icon_skill.png" alt="">
+              </div>
+              <div class="skill-desc">
+                <h3 class="skill-desc-title">Python</h3>
+                <div class="rating">
+                  <div class="rate rate3"></div>
+                </div>
+                <div class="section-text">
+                  ただいま準備中です。</div>
+              </div>
+            </div>
+            <div class="card">
+              <div class="skill-icon">
+                <img src="img/skill/icon_skill.png" alt="">
+              </div>
+              <div class="skill-desc">
+                <h3 class="skill-desc-title">Docker</h3>
+                <div class="rating">
+                  <div class="rate rate2"></div>
+                </div>
+                <div class="section-text">
+                  ただいま準備中です。</div>
+              </div>
+            </div>
+            <div class="card">
+              <div class="skill-icon">
+                <img src="img/skill/icon_skill.png" alt="">
+              </div>
+              <div class="skill-desc">
+                <h3 class="skill-desc-title">PHP</h3>
+                <div class="rating">
+                  <div class="rate rate3"></div>
+                </div>
+                <div class="section-text">
+                  ただいま準備中です。</div>
+              </div>
+            </div>
+            <div class="card">
+              <div class="skill-icon">
+                <img src="img/skill/icon_skill.png" alt="">
+              </div>
+              <div class="skill-desc">
+                <h3 class="skill-desc-title">Adobe Premiore pro</h3>
+                <div class="rating">
+                  <div class="rate rate3"></div>
+                </div>
+                <div class="section-text">
+                  ただいま準備中です。
+                </div>
+              </div>
+            </div>
+            <div class="card">
+              <div class="skill-icon">
+                <img src="img/skill/icon_skill.png" alt="">
+              </div>
+              <div class="skill-desc">
+                <h3 class="skill-desc-title">Perl</h3>
+                <div class="rating">
+                  <div class="rate rate3"></div>
+                </div>
+                <div class="section-text">
+                  ただいま準備中です。
+                </div>
+              </div>
+            </div>
+          </div>
+           <div class="card">
+              <div class="skill-desc">
+                <h3 class="skill-desc-title"><span class="text-bold">QUALIFICATION</span></h3>
+                <div class="section-text">
+                 <ul>
+                  <li> 基本情報資格者試験 合格</li>
+                  <li> aws solution architect associate</li>
+                  <li> TOEIC 750 (2013)</li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          </div>
+      </section>
+      <section class="section" id="other">
+        <div class="inner">
+          <div class="section-title-area">
+            <h2 class="section-title">OTHER ACTIVITY</h2>
+            <div class="section-intro">
+              <a href="https://qiita.com/Ponjiro">Qiita</a> : 放置中<br>
+              <a href="https://note.com/ponjiros">note</a> : 作成したのみ<br>
+              <a href="https://www.youtube.com/channel/UCuGGkyLwsSfiZJQZ_iCXQTQ">youtube</a> : 放置中<br>
+              <a href="https://github.com/takanori-ops">Github</a> : 個人のプロジェクトや技術調査用サンプルプログラム<br>
+            </div>
+          </div>
+          <div class="section-body">
+            <div class="section-image">
+              <img class="lazyload fadein" data-src="img/about/profile.jpg" alt="">
+            </div>
+            <div class="section-desc">
+              <div class="section-text">
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section class="section" id="contact">
+        <div class="inner">
+          <div class="section-title-area">
+            <h2 class="section-title">CONTACT</h2>
+            <div class="section-intro">
+              お問い合わせは、SNSかメールでお願いいたします。
+            </div>
+          </div>
+          <div class="section-body contact-wrapper">
+            <div class="contact-list">
+              <ul>
+                <li><a href="https://twitter.com/takanor34240298" target="_blank"><img src="img/contact/icon_tw.png" alt=""></a>
+                </li>
+                <li><a href="https://www.facebook.com/" target="_blank"><img src="img/contact/icon_fb.png" alt=""></a>
+                </li>
+              </ul>
+              <div class="contact-other">takanori2040@gmail.com</div>
+
+            </div>
+          </div>
+        </div>
+      </section>
+
+    </main>
+    <footer id="footer">
+      <div class="pageTop">
+        <div id="js-pageTop">
+          Page Top
+        </div>
+      </div>
+      <div class="inner">
+        <div class="copyright">Copyright&copy;Takanori. All Rights Reserved.</div>
+        </div>
+    </footer>
+  </div>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
 </script>
 
 <style>
@@ -25,4 +316,5 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+
 </style>
